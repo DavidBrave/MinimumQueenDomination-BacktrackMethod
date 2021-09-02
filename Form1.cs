27941a1d529cfaa307355326b58a904cf3882c8d
DavidBrave <davidbrave244@gmail.com>
2021-09-02 13:39:36 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinimumQueenDomination_BacktrackMethod
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+
+        bool[,] board;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
